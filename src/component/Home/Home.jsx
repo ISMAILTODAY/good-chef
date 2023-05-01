b372dce392bbd14c1aafa1cd0ceb0a6da6cde6e8
ISMAILTODAY <ismail878708@gmail.com>
2023-05-01 22:57:01 +0600

    complete navbar

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -2,8 +2,20 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div>
-            <h1>this is Home</h1>
+        <div className=' bg-[#a7dbd8] '>
+            <div className='p-10 flex items-center justify-evenly '>
+                <h1 className='text-5xl font-bold'>
+                    <h1 className='mb-5 ml-[-100px]'>
+                        <span className='text-[#ff55c2]'>Good</span>
+                        food is the</h1>
+                    <span className='text-[#ff55c2]'>foundation</span>
+                    of <h1 className='mt-5 ml-[100px]'>genuine <span className='text-[#ff55c2]'>
+                        happiness</span>
+                    </h1>
+                </h1>
+                <img className='w-[40%] rounded-lg shadow-lg' src="./../../../public/image/chinese-food.png" alt="" />
+            </div>
+
         </div>
     );
 };
--- src/component/NavigationBar/NavigationBar.jsx
+++ src/component/NavigationBar/NavigationBar.jsx
@@ -3,7 +3,15 @@ import React from 'react';
 const NavigationBar = () => {
     return (
         <div>
-
+            <div className=' bg-black text-zinc-50 px-10 flex justify-between items-center'>
+                <h1 className='text-3xl font-semibold'><span className='text-[#ff55c2] ' >G</span>ood <span className='text-[#ff55c2] ' >C</span>hef</h1>
+                <ul className='flex items-center  gap-10'>
+                    <li>Home</li>
+                    <li>Blog</li>
+                    <li><img className='rounded-full w-[50px]' src="https://cdn-edcnk.nitrocdn.com/PSaIWXjqkelWPpnJnijqymZXwpeyImuQ/assets/images/optimized/rev-0fc88f9/wp-content/uploads/2021/10/Being-Simple.jpg" alt="" /></li>
+                    <li><button>Login</button></li>
+                </ul>
+            </div>
         </div>
     );
 };
