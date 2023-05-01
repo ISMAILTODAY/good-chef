07e1236bc8b119b6e84389be852f61d19b2d15a3
ISMAILTODAY <ismail878708@gmail.com>
2023-05-01 21:28:30 +0600

    initial setup is done

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCn4ICxMcM_7tGG91uWvAbaiti0x-bIhts",
+    authDomain: "good-chef-41eb9.firebaseapp.com",
+    projectId: "good-chef-41eb9",
+    storageBucket: "good-chef-41eb9.appspot.com",
+    messagingSenderId: "911648298544",
+    appId: "1:911648298544:web:7d74b01ca2ee3dcc21fff5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
