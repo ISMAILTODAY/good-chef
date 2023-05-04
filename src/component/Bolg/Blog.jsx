8258b004e98a35482f1f021381f504ac3e6f80e2
ISMAILTODAY <ismail878708@gmail.com>
2023-05-04 16:15:22 +0600

    add react to pdf download button

--- src/component/Bolg/Blog.jsx
+++ src/component/Bolg/Blog.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import Pdf from "react-to-pdf";
+
+const ref = React.createRef();
 
 const Blog = () => {
     return (
-        <div className='p-5 mt-5'>
+        <div ref={ref} className='p-5 mt-5'>
             <div className='shadow-lg p-3 rounded'>
                 <h1>01. What is the differences between uncontrolled and controlled components?</h1>
                 <p>In React, controlled components refer to components that have their state and behavior controlled by the parent component. These components rely on props passed down from the parent component to update their state and behavior. Uncontrolled components refer to components that manage their own state internally.</p>
@@ -28,6 +31,16 @@ const Blog = () => {
                 <h1>04. What is a custom hook, and why will you create a custom hook?</h1>
                 <p>Custom Hooks are a mechanism to reuse stateful logic. Such as setting up a subscription and remembering the current value. <br />The main reason to write a custom hook is for code reusability and fot that we create custom hook.</p>
             </div>
+
+
+            <div className='d-flex justify-content-center mt-5'>
+                <Pdf targetRef={ref} filename="code-example.pdf">
+                    {({ toPdf }) => <button className='btn btn-primary' onClick={toPdf}>Download Pdf</button>}
+                </Pdf>
+            </div>
+
+
+
         </div>
     );
 };
